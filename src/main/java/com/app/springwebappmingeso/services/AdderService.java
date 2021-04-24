b3e697576f85a90f310203f7bcd880f51073552a
Dennis <dennis.urrutia@usach.cl>
2021-04-23 21:03:26 -0400

    feat: Adder & AdderService

--- src/main/java/com/app/springwebappmingeso/services/AdderService.java
+++ src/main/java/com/app/springwebappmingeso/services/AdderService.java
@@ -1,4 +1,23 @@
 package com.app.springwebappmingeso.services;
+import com.app.springwebappmingeso.models.Adder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
 
+@RestController
+@RequestMapping("/adder")
+@CrossOrigin(origins = "*")
 public class AdderService {
+
+    private final Gson gson;
+
+    public Integer calculateSum(@RequestBody String request){
+        Adder adder = gson.fromJson(request, Adder.class);
+        Integer result = calculateSum(adder.getNumber1(), adder.getNumber2());
+        adder.setResult(result);
+        return result;
+    }
 }
