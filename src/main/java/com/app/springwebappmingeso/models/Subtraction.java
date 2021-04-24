b4407a93894ba4fae7712f84ea06c98f52d50872
Dennis <dennis.urrutia@usach.cl>
2021-04-24 16:15:29 -0400

    feat: Model Subtraction & fix: AdderService

---
+++ src/main/java/com/app/springwebappmingeso/models/Subtraction.java
@@ -0,0 +1,42 @@
+package com.app.springwebappmingeso.models;
+
+public class Subtraction {
+    private Integer number1;
+    private Integer number2;
+    private Integer result;
+
+    public Subtraction(Integer number1, Integer number2, Integer result) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.result = result;
+    }
+
+    public Integer getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(Integer number1) {
+        this.number1 = number1;
+    }
+
+    public Integer getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(Integer number2) {
+        this.number2 = number2;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public Integer calculateSubtraction(Integer number1, Integer number2){
+        Integer result = number1 - number2;
+        return result;
+    }
+}
--- src/main/java/com/app/springwebappmingeso/services/AdderService.java
+++ src/main/java/com/app/springwebappmingeso/services/AdderService.java
@@ -1,10 +1,8 @@
 package com.app.springwebappmingeso.services;
 import com.app.springwebappmingeso.models.Adder;
+import com.google.gson.GsonBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 
 @RestController
@@ -14,9 +12,13 @@ public class AdderService {
 
     private final Gson gson;
 
+    AdderService(){
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+    @PostMapping("/sum")
     public Integer calculateSum(@RequestBody String request){
         Adder adder = gson.fromJson(request, Adder.class);
-        Integer result = calculateSum(adder.getNumber1(), adder.getNumber2());
+        Integer result = adder.calculateSum(adder.getNumber1(), adder.getNumber2());
         adder.setResult(result);
         return result;
     }
