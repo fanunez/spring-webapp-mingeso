6fcb799b526dcba298c934a5e4a66035f2b8aecd
Dennis <dennis.urrutia@usach.cl>
2021-04-25 16:20:20 -0400

    fix: AdderService

--- src/main/java/com/app/springwebappmingeso/services/AdderService.java
+++ src/main/java/com/app/springwebappmingeso/services/AdderService.java
@@ -1,6 +1,7 @@
 package com.app.springwebappmingeso.services;
 import com.app.springwebappmingeso.models.Adder;
 import com.google.gson.GsonBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
@@ -16,10 +17,11 @@ public class AdderService {
         this.gson = new GsonBuilder().setPrettyPrinting().create();
     }
     @PostMapping("/sum")
-    public Integer calculateSum(@RequestBody String request){
+    public ResponseEntity<String> calculateSum(@RequestBody String request){
+        System.out.println(request);
         Adder adder = gson.fromJson(request, Adder.class);
         Integer result = adder.calculateSum(adder.getNumber1(), adder.getNumber2());
         adder.setResult(result);
-        return result;
+        return new ResponseEntity<>(gson.toJson(adder), HttpStatus.OK);
     }
 }
