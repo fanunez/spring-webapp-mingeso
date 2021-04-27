cdd1b993208d6adcd41167cc46ba00c2916fbbc6
Dennis <dennis.urrutia@usach.cl>
2021-04-27 17:42:56 -0400

    fix: code issues

--- src/test/java/com/app/springwebappmingeso/CalculateTest.java
+++ src/test/java/com/app/springwebappmingeso/CalculateTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculateTest {
+class CalculateTest {
     @Test
     void calculateSum_twoNumbers_returnNumber() {
         Calculate calculate = new Calculate(1, 2, null);
